ab5ebbafd00e57f80190df41fd17a1d05169f37d
sasu4 <lubomir.benko@gmail.com>
2025-03-09 23:50:03 +0100

    added link to home on logo

--- trail-test/src/NavbarExplorer.jsx
+++ trail-test/src/NavbarExplorer.jsx
@@ -84,7 +84,7 @@ function NavbarExplorer() {
       <div className='bg-[#416A65] px-0 py-3 py-lg-0'>
         <nav className="d-flex justify-content-between align-items-center px-3 px-lg-0 py-lg-0 offset-lg-2 col-lg-8">
           <div className="py-0 py-lg-3">
-            <img src={avatar_white} alt="avatar_white" className='' width={80} />
+            <a href={`${basePath}`}><img src={avatar_white} alt="avatar_white" className='' width={80} /></a>
           </div>
           <div className="d-flex align-items-center">
             {/* Language Dropdown */}
