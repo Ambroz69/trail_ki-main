eb0c6e14db73b6fbaca1cbe8faf1c46330f50315
sasu4 <lubomir.benko@gmail.com>
2025-05-15 20:26:22 +0200

    revert render disk integration changes

--- backend/routes/trailsRoute.js
+++ backend/routes/trailsRoute.js
@@ -15,7 +15,8 @@ const router = express.Router();
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const isAudio = file.mimetype.startsWith('audio/');
-    const folder = isAudio ? '/uploads/audio/' : '/uploads/';
+    const folder = isAudio ? 'uploads/audio/' : 'uploads/';
+    //const folder = isAudio ? '/uploads/audio/' : '/uploads/'; //render disk changes
     fs.mkdirSync(folder, { recursive: true }); // ensures path exists
     cb(null, folder);
   },
